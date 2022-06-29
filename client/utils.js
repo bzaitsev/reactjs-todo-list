748bf6bcece72174b3eae0dab5a872fb85553360
boris_zaytsev <bizaytsev@gmail.com>
2022-06-29 15:20:54 +0300

    dev optimization

--- client/utils.js
+++
@@ -1,4 +0,0 @@
-/*
-utils.getTodoById(todos, listId);
-todos.filter(todo => todo.id === listId)[0];
-*/
--- webpack.config.js
+++ webpack.config.js
@@ -6,70 +6,70 @@ const CLIENT_PATH = path.resolve(__dirname, 'client'),
       PUBLIC_PATH = path.resolve(__dirname, 'docs'),
       normalizeCssPath = path.resolve(__dirname, 'node_modules/normalize.css/');
 
-let config = {
-  entry: path.resolve(CLIENT_PATH, 'index.jsx'),
-  output: {
-    path: PUBLIC_PATH,
-    filename: 'build/app.js',
-    publicPath: '/'
-  },
-  devServer: {
-    static: {
-      directory: PUBLIC_PATH
-    },
-    historyApiFallback: true
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'build/app.css'
-    }),
-    new HtmlWebPackPlugin({
-      template: path.resolve(CLIENT_PATH, 'index.html'),
-      filename: "index.html"
-    })    
-  ],  
-  module: {
-    rules: [
-      {
-        test: /\.(gif|png|jpe?g|svg|ico|eot|ttf|woff|woff2|otf)$/i,
-        include: [
-          CLIENT_PATH
-        ],
-        type: 'asset/resource'
-      }, {
-        test: /\.(js|jsx)$/,
-        include: [
-          CLIENT_PATH
-        ],
-        loader: 'babel-loader'
-      }, {
-        test: /\.scss$|\.css$/,
-        include: [
-          CLIENT_PATH,
-          normalizeCssPath
-        ],
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ],
-      }, {
-        test: /\.html$/,
-        include: [
-          CLIENT_PATH
-        ],        
-        loader: "html-loader"
-      }
-    ] 
-  }
-};
-
 module.exports = (env, options) => {
   let production = options.mode === 'production';
   
-  config.devtool = production
-    ? 'source-map'
-    : 'eval-source-map';
+  let config = {
+    entry: path.resolve(CLIENT_PATH, 'index.jsx'),
+    devtool: production ? 'source-map': 'eval-source-map',
+    output: {
+      path: PUBLIC_PATH,
+      filename: 'build/app.js',
+      publicPath: '/'
+    },
+    devServer: {
+      static: {
+        directory: PUBLIC_PATH
+      },
+      historyApiFallback: true
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: path.resolve(CLIENT_PATH, 'index.html'),
+        filename: "index.html"
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(gif|png|jpe?g|svg|ico|eot|ttf|woff|woff2|otf)$/i,
+          include: [
+            CLIENT_PATH
+          ],
+          type: 'asset/resource'
+        }, {
+          test: /\.(js|jsx)$/,
+          include: [
+            CLIENT_PATH
+          ],
+          loader: 'babel-loader'
+        }, {
+          test: /\.scss$|\.css$/,
+          include: [
+            CLIENT_PATH,
+            normalizeCssPath
+          ],
+          use: [
+            production ? MiniCssExtractPlugin.loader: 'style-loader',
+            "css-loader",
+            "sass-loader"
+          ],
+        }, {
+          test: /\.html$/,
+          include: [
+            CLIENT_PATH
+          ],
+          loader: "html-loader"
+        }
+      ]
+    }
+  };
+
+  if (production) {
+    config.plugins.push(new MiniCssExtractPlugin({
+      filename: 'build/app.css'
+    }));
+  }
 
   return config;
 };
